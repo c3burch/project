3d35504bc8303dfd2cb97edf172071cf4df8094f
Christina Burch <cburch@bio.unc.edu>
2012-11-29 14:40:41 -0500

    added thorough documentation

--- mean.py
+++ mean.py
@@ -5,12 +5,15 @@ import sys
 # sys.argv is an array of inputs to the command line
 # here sys.argv[0] input is mean.py
 
+#checks to make sure there are inputs
 if len(sys.argv) == 1:
     print 'Error: No arguments given.'
     exit()
 
+#takes the mean of all inputs
 sum = 0
 for num in sys.argv[1:]:
     sum += float(num)
 
+#prints the mean
 print sum / (len(sys.argv) - 1)
