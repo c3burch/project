8a5e65eac92644c3c7b9ff4e0879a61fb564f1d1
Christina Burch <cburch@bio.unc.edu>
2012-11-29 14:06:05 -0500

    Handled case with no arguments.

--- mean.py
+++ mean.py
@@ -2,7 +2,12 @@
 
 import sys
 
-#sys.argv is an array of inputs to the command line
+# sys.argv is an array of inputs to the command line
+# here sys.argv[0] input is mean.py
+
+if len(sys.argv) == 1:
+    print 'Error: No arguments given.'
+    exit()
 
 sum = 0
 for num in sys.argv[1:]:
